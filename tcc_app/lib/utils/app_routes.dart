cc1c310d3c36c439f5a906b34337098f4322d9ee
Joao-Henrique0 <joaohenriquecarvalhosoares@gmail.com>
2025-08-22 20:32:08 -0300

    tabulçaõ dos dados

--- tcc_app/lib/utils/app_routes.dart
+++
@@ -1,9 +0,0 @@
-class AppRoutes {
-  static const login = '/login';
-  static const register = '/register';
-  static const home = '/';
-  static const taskForm = '/task-form';
-  static const chatBot = '/chatBot';
-  static const notiSettings = '/noti-settings';
-
-}
